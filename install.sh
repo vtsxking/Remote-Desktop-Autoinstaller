9de7f578ab6ce5209e861ac7822a63e287951de2
vtsxking <kennyrosasmondragon@gmail.com>
2021-04-29 13:29:42 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,4 +6,4 @@ apt upgrade
 wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
 apt install ./chrome-remote-desktop_current_amd64.deb
 apt install ubuntu-mate-desktop
-DISPLAY= /opt/google/chrome-remote-desktop/start-host --code=$Act_Code --redirect-url="https://remotedesktop.google.com/_/oauthredirect" --name=$(hostname)
+DISPLAY= /opt/google/chrome-remote-desktop/start-host --code="$Act_Code" --redirect-url="https://remotedesktop.google.com/_/oauthredirect" --name=$(hostname)
