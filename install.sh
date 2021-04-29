f3d6aafd3926ee5599965b214c7add907dc4cebc
vtsxking <kennyrosasmondragon@gmail.com>
2021-04-29 03:41:54 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,22 +1,9 @@
-  
 #!/bin/bash
 
-if [$# == 0];
-then
-    echo "Enter your activation code..."
-fi
-
-if [$# == 1];
-then
-    apt update
-    apt upgrade
-    wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
-    apt install ./chrome-remote-desktop_current_amd64.deb
-    apt install ubuntu-mate-desktop
-    DISPLAY= /opt/google/chrome-remote-desktop/start-host --code=$1 --redirect-url="https://remotedesktop.google.com/_/oauthredirect" --name=$(hostname)
-fi
-
-if [$# > 2];
-then
-    echo "too many args..."
-fi
+read -e -p "Enter Activation Code: " Act_Code
+apt update
+apt upgrade
+wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
+apt install ./chrome-remote-desktop_current_amd64.deb
+apt install ubuntu-mate-desktop
+DISPLAY= /opt/google/chrome-remote-desktop/start-host --code=$Act_Code --redirect-url="https://remotedesktop.google.com/_/oauthredirect" --name=$(hostname)
