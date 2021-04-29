10a44e60f4fdbbb9b26da6b818e953f5ec20ba6b
vtsxking <kennyrosasmondragon@gmail.com>
2021-04-29 18:24:28 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,4 +8,4 @@ wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
 apt install ./chrome-remote-desktop_current_amd64.deb
 apt install ubuntu-mate-desktop
 read -e -p "$CYAN  Enter Activation Code: $COL_RESET" Act_Code
-DISPLAY= /opt/google/chrome-remote-desktop/start-host --code=\"$Act_Code\" --redirect-url=\"https://remotedesktop.google.com/_/oauthredirect\" --name=$(hostname)
+DISPLAY= /opt/google/chrome-remote-desktop/start-host --code=\"$Act_Code\" --redirect-url=\"https://remotedesktop.google.com/_/oauthredirect\" --name=\$(hostname)
