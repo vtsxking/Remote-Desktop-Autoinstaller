82e6504d5d281fe067d8be42acf7f884db576497
vtsxking <kennyrosasmondragon@gmail.com>
2021-04-29 03:38:53 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,12 @@
+  
+#!/bin/bash
 
-
-if [$# -eq 0];
+if [$# == 0];
 then
     echo "Enter your activation code..."
 fi
 
-if [$# -eq 1];
+if [$# == 1];
 then
     apt update
     apt upgrade
@@ -15,7 +16,7 @@ then
     DISPLAY= /opt/google/chrome-remote-desktop/start-host --code=$1 --redirect-url="https://remotedesktop.google.com/_/oauthredirect" --name=$(hostname)
 fi
 
-if [$# -gt 2];
+if [$# > 2];
 then
     echo "too many args..."
 fi
