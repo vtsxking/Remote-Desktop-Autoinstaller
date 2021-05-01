32dfc7728cde605e6867029945cf70089d57cec7
vtsxking <kennyrosasmondragon@gmail.com>
2021-05-01 03:09:22 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,6 +10,7 @@ useradd $(hostname) -s /bin/bash -p '*'
 adduser $(hostname) sudo
 
 echo -e "$CYAN Copying Remote Installer... $COL_RESET"
+mkdir /tmp/remote-desktop
 cp remote-install.sh /tmp/remote-desktop/remote-install.sh
 
 echo -e "$CYAN Switch User... $COL_RESET"
